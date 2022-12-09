4c60bd7e77cfe500641443fd33c587bd112ebd59
hussain <theguyiscoming@gmail.com>
2022-12-09 22:18:17 +0000

    initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %ld byte(s)\n",(unsigned long)sizeof(b));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-}	
+}
