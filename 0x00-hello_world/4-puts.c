0e2521f6271ab47b9577a284634f6658e84ea2b0
hussain <theguyiscoming@gmail.com>
2022-12-09 15:24:53 +0000

    intial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-/*
+
+/**
  * main - entry point
  *
  * return: Always 0 (success)
  */
-
 int main(void)
 {
+
 	puts("\"Progamming is like building a multilingual puzzle");
 	return (0);	
 
 }
-
