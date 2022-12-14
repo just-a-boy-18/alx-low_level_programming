40e7cfbdf5e4ce4b46d7868f5efd24589d9fa94c
hussain <theguyiscoming@gmail.com>
2022-12-14 00:59:55 +0000

    initial commit

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,1 +1,1 @@
-char -putchar(char c);                                                                                void print_alphabet(void);                                                                            void print_alphabet_x10(void)  
+
