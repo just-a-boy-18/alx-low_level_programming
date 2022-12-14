eadea0492d766b64b775f95a565317dcbb4a2de5
hussain <theguyiscoming@gmail.com>
2022-12-14 16:50:56 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - a function that absolute value of an integer
+ * @j: integer input
+ * Return: absolute value of j
+ */
+int _abs(int j);
+{
+	return (j * ((j > 0) - (j < 0)));
+}
