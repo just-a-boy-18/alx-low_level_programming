b911133f0c4c759013484bee877968fdc30c10f1
hussain <theguyiscoming@gmail.com>
2022-12-14 23:48:16 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/*
+ * jack_bauer - function that prints every minute of the day , 24 hours clock
+ * h = hour, m = minutes
+ * Return: 24 hour clock line by line
+ */
+void jack_bauer(void)
+{
+	for (int h = 0; h < 24; h++)
+	{
+		for (int m = 0; m < 60; m++)
+		{
+		_putchar('0' + (h / 10));
+		_putchar('0' + (h % 10));
+		_putchar(':');
+		_putchar('0' + (m / 10));
+		_putchar('0' + (m % 10));
+		_putchar('\n');
+
+		}
+
+
+
+	}
+
+
+
+}
