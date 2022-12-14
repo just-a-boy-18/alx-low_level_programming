e1c41d0a6f452748f13b9c3292233188b7e6cb21
hussain <adamuhussain22@gmail.com>
2022-12-14 14:18:52 +0100

    hot fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,8 @@
 /**
  * print_sign - print teh sign of a number
  *
- * Return: 1 prints '+' if (n > 0), 0 print '0' if (n = 0), -1 print '-' if (n < 0)
+ * Return: 1 prints '+' if (n > 0), 0 print '0' if (n = 0), 
+ * -1 print '-' if (n < 0)
  */
 int print_sign(int n)
 {
@@ -12,7 +13,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	
+
 	else if (n == 0)
 	{
 		_putchar('0');
