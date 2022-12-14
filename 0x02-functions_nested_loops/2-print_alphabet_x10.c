802d5294b7d991465ea16ee83f6420d01566bd26
hussain <theguyiscoming@gmail.com>
2022-12-14 01:18:11 +0000

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,18 +7,19 @@
 void print_alphabet_x10(void)
 {
 	int n, ba;
+
 	ba = 0;
-	
+
 	while (ba < 10)
 	{
 		for (n = 'a'; n <= 'z'; n++)
 		{
 			_putchar(n);
-		
+
 		}
 		ba++;
 		_putchar('\n');
-	
+
 	}
 
-}	
+}
