41e7bce837ae6c7948dca6736775d5ae8bbc02ba
hussain <theguyiscoming@gmail.com>
2022-12-16 19:42:18 +0000

    initial commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
-/*
- * _isdigit - check for a digit (0 through 9)
+/**
+ * _isdigit -  a function that checks for a digit 0-9
  * @c: input
  * Return: 1 if c is a digit , 0 otherwise
  */
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - a function that multiplies two integers
+ * @a: input
+ * @b: input
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	printf("%d * %d\n", a, b);
+
+	return (0);
+
+
+}
